f8f2266c8e08a47f4992afde9d0bc5ef80158db7
kartik855 <kartiknandanwar23@gmail.com>
2026-06-04 21:43:44 +0530

    Update create-order.js

--- lastnight-deploy/api/create-order.js
+++ lastnight-deploy/api/create-order.js
@@ -1,1 +1,19 @@
+import Razorpay from "razorpay";
 
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+export default async function handler(req, res) {
+  try {
+    const order = await razorpay.orders.create({
+      amount: 9900,
+      currency: "INR",
+    });
+
+    res.status(200).json(order);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
